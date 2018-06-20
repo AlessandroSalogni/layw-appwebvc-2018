59bc4d78907ff3f0307608afaef3f25a37044ead
riccardoperotti <20013787@studenti.uniupo.it>
2018-06-20 13:15:20 +0200

    Completamento oAuth: post del token generato su homestation. Costruzione dei file di configurazione che contengono l'IP della homestation e i codici dell'app fitbit

---
+++ Layw-web/Configuration/HomestationIP.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Layw_web.Configuration
+{
+    public class HomestationIP
+    {
+        public string IP { get; set; }
+        public int Port { get; set; }
+        public string Url { get; set; }
+
+        public string getTotalUrl()
+        {
+            return "http://" + IP + ":" + Port + Url; 
+        }
+    }
+}
--- Layw-web/Startup.cs
+++ Layw-web/Startup.cs
@@ -1,16 +1,33 @@
-﻿using Microsoft.AspNetCore.Authentication.Cookies;
+﻿using Layw_web.Configuration;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authentication.OAuth;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
 
 namespace FitbitAuth
 {
     public class Startup
     {
+        private static readonly HttpClient client = new HttpClient();
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
         public void ConfigureServices(IServiceCollection services)
         {
+            services.Configure<HomestationIP>(Configuration.GetSection("homestation-ip"));
+            services.Configure<FitbitCodes>(Configuration.GetSection("fitbit-codes"));
+
             services.AddAuthentication(options =>
             {
                 options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
@@ -24,10 +41,12 @@ namespace FitbitAuth
 
             .AddFitbit(options =>
             {
+                var configurationFitbit = Configuration.GetSection("fitbit-codes").Get<FitbitCodes>();
+
                 // https://dev.fitbit.com/build/reference/web-api/oauth2/
                 // https://dev.fitbit.com/
-                options.ClientId = "22CT2W";
-                options.ClientSecret = "431a34ca17c79d6bc368f86d5fcc8f73";
+                options.ClientId = configurationFitbit.ClientId;
+                options.ClientSecret = configurationFitbit.ClientSecret;
 
                 // Importante per avere accesso all'access token.
                 options.SaveTokens = true;
@@ -38,6 +57,18 @@ namespace FitbitAuth
                     {
                         // Qui è possibile salvare il token.
                         var accessToken = context.AccessToken;
+
+                        var values = new Dictionary<string, string>
+                        {
+                           { "accessToken", accessToken },
+                        };
+
+                        var configurationIP = Configuration.GetSection("homestation-ip").Get<HomestationIP>();
+                        var content = new FormUrlEncodedContent(values);
+                        var response = await client.PostAsync(configurationIP.getTotalUrl(), content);
+                        var responseString = await response.Content.ReadAsStringAsync();
+
+                        Console.WriteLine(responseString);
                     }
                 };
             });
