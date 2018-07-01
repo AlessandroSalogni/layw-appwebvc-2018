d9cc7a77e04833fa56765621b6426978b6799125
riccardoperotti <20013787@studenti.uniupo.it>
2018-07-02 00:18:23 +0200

    Aggiunta nuovo progetto e refactoring nomi progetti

---
+++ LaywAuthentication/Configuration/FitbitCodes.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LaywAuthentication.Configuration
+{
+    public class FitbitCodes
+    {
+        public string ClientId { get; set; }
+        public string ClientSecret { get; set; }
+    }
+}
---
+++ LaywAuthentication/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LaywAuthentication.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet("~/")]
+        public IActionResult Index() => View();
+    }
+}
