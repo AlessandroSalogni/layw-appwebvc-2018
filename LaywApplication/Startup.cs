7f087123245274e9f6d36cc43cfa1de44cf143f6
riccardoperotti <20013787@studenti.uniupo.it>
2018-08-07 13:53:05 +0200

    Minimo restyling autenticazione

--- LaywApplication/Startup.cs
+++ LaywApplication/Startup.cs
@@ -43,22 +43,25 @@ namespace LaywApplication
                  var configurationGoogle = Configuration.GetSection("google-codes").Get<GoogleCodes>();
                  options.ClientId = configurationGoogle.ClientId;
                  options.ClientSecret = configurationGoogle.ClientSecret;
-                 
+
                  options.Events = new OAuthEvents
                  {
                      OnCreatingTicket = async context =>
                      {
-                         //todo cercare se c'è un modo migliore per recuperare dati
-                         string email = context.Identity.Claims.FirstOrDefault(c => c.Type == "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress").Value;
-                         string name = context.Identity.Claims.FirstOrDefault(c => c.Type == "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/name").Value;
+                         if (context.Identity.AuthenticationType.Equals("Google")) //A seconda del tipo cambiano gli url sotto. cercare un modo migliore di quegli url
+                         {
+                             //todo cercare se c'è un modo migliore per recuperare dati
+                             string email = context.Identity.Claims.FirstOrDefault(c => c.Type == "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress").Value;
+                             string name = context.Identity.Claims.FirstOrDefault(c => c.Type == "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/name").Value;
 
-                         //todo Gestire che il medico esista già con una get che ancora non c'è
-                         string json = "{\"doctor\": {\"name\": \"" + name + "\", \"email\": \"" + email + "\"}}";
-                         Utils.Post("http://localhost:4567/api/v1.0/doctors", json);
+                             //todo Gestire che il medico esista già con una get che ancora non c'è
+                             string json = "{\"doctor\": {\"name\": \"" + name + "\", \"email\": \"" + email + "\"}}";
+                             Utils.Post("http://localhost:4567/api/v1.0/doctors", json);
+                         }
                      }
                  };
              });
-
+             
             services.AddMvc();
         }
 
