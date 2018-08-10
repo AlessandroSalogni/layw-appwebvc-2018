7060a35701117d8b49a0e08093fff56458ca50a5
AlessandroSalogni <20013907@studenti.uniupo.it>
2018-08-10 09:38:36 +0200

    Fatto redirect su signin nella dashboard se un utente non è autenticato

--- LaywApplication/Controllers/DashboardController.cs
+++ LaywApplication/Controllers/DashboardController.cs
@@ -25,24 +25,29 @@ namespace LaywApplication.Controllers
         [HttpGet("~/dashboard")]
         public IActionResult Index()
         {
-            Doctor doctor = new Doctor(
-                User.Claims.FirstOrDefault(c => c.Type == "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress").Value, 
+            if (User?.Identity?.IsAuthenticated ?? false)
+            {
+                Doctor doctor = new Doctor(
+                User.Claims.FirstOrDefault(c => c.Type == "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress").Value,
                 User.Claims.FirstOrDefault(c => c.Type == "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/name").Value,
                 new Uri(User.Claims.FirstOrDefault(c => c.Type == "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/uri").Value));
 
-            string jsonResult = "{\"doctor\": {\"name\": \"" + doctor.Name + "\", \"email\": \"" + doctor.EMail + "\"}}";
-            Utils.Post(config.Value.GetTotalUrl() + "doctors", jsonResult);
+                string jsonResult = "{\"doctor\": {\"name\": \"" + doctor.Name + "\", \"email\": \"" + doctor.EMail + "\"}}";
+                Utils.Post(config.Value.GetTotalUrl() + "doctors", jsonResult);
 
-            jsonResult = Utils.Get(config.Value.GetTotalUrl() + "users?doctor-id=" + doctor.EMail);
-            JObject json = JObject.Parse(jsonResult);
-            JArray jsonArray = (JArray)json.GetValue("users");
+                jsonResult = Utils.Get(config.Value.GetTotalUrl() + "users?doctor-id=" + doctor.EMail);
+                JObject json = JObject.Parse(jsonResult);
+                JArray jsonArray = (JArray)json.GetValue("users");
 
-            foreach (JObject obj in jsonArray)
-                doctor.Patients.Add(JsonConvert.DeserializeObject<Patient>(obj.ToString()));
+                foreach (JObject obj in jsonArray)
+                    doctor.Patients.Add(JsonConvert.DeserializeObject<Patient>(obj.ToString()));
 
-            //Ritorna la view della cartella home (ovvero la cartella che ha lo stesso prefisso di questo controller)
-            //di nome Index, ovvero la view che ha lo stesso nome della action del controller
-            return View(doctor);
+                //Ritorna la view della cartella home (ovvero la cartella che ha lo stesso prefisso di questo controller)
+                //di nome Index, ovvero la view che ha lo stesso nome della action del controller
+                return View(doctor);
+            }
+            else
+                return Redirect("~/signin");
         }
     }
 }
