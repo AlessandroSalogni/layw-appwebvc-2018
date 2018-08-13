52dae8f00ca05ad26ab0c2f62e54cd0caf19c10f
riccardoperotti <20013787@studenti.uniupo.it>
2018-08-13 20:51:55 +0200

    Sperimentazioni varie.

--- LaywApplication/Controllers/APIUtils/Utils.cs
+++ LaywApplication/Controllers/APIUtils/Utils.cs
@@ -1,11 +1,48 @@
-﻿using System.Net;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using RestSharp;
+using System.Collections.Generic;
+using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace LaywApplication.Controllers.APIUtils
 {
-    public static class Utils
+    public static class Utils   
     {
+        private const string dateFormat = "dd-MM-yyyy";
+
+        public static List<T> Get<T>(string baseUrl, string url, string root) where T : class, new()
+        {
+            var client = new RestClient(baseUrl);
+            var rq = new RestRequest(url, Method.GET);
+            var rs = client.Execute(rq);
+
+            JObject json = JObject.Parse(rs.Content);
+            JArray jsonArray = (JArray)json.GetValue(root);
+
+            List<T> list = new List<T>();
+            foreach (JObject obj in jsonArray)
+                list.Add(JsonConvert.DeserializeObject<T>(obj.ToString()));
+            
+            return list;
+        }
+
+        /*
+        public static string Post(string baseUrl, string url, string json)
+        {
+            var client = new RestClient(baseUrl);
+            var request = new RestRequest(url, Method.POST)
+            {
+                RequestFormat = DataFormat.Json
+            };
+            request.AddBody(json);
+
+            var response = client.Execute(request);
+            return response.Content;
+        }
+        */
+        
         private static readonly HttpClient client = new HttpClient();
 
         public static string Get(string uri)
@@ -33,5 +70,8 @@ namespace LaywApplication.Controllers.APIUtils
             }
             return result;
         }
+        
+
+
     }
 }
