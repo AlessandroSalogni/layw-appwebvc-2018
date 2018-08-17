a8395dc28628eb31c8ddd98c1fb4bdbf285032fe
riccardoperotti <20013787@studenti.uniupo.it>
2018-08-17 13:02:37 +0200

    Istogramma per ogni paziente indicante gli ultimi passi di tutti i giorni

--- LaywApplication/Controllers/GoalStepsDailyController.cs
+++ LaywApplication/Controllers/GoalStepsDailyController.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using LaywApplication.Configuration;
 using LaywApplication.Controllers.APIUtils;
 using LaywApplication.Models;
@@ -34,7 +33,7 @@ namespace LaywApplication.Controllers
             List<AchievedGoals> agList = new List<AchievedGoals>();
             
             foreach (Patient patient in DashboardController.doctor.Patients)
-                agList.Add(GetAchievedGoals(patient.Id));
+                agList.Add(GetAchievedGoals(patient.Id, DateTime.Now.ToShortDateString().Replace('/','-')));
             
             int notAchieved = agList.Count(x => x.GoalsStepsDaily.Goal > x.ActivitySummary.Steps);
             int ac = agList.Count - notAchieved;
@@ -50,26 +49,37 @@ namespace LaywApplication.Controllers
                 return Json(new List<object> { new { category = "Achieved", amount = ac, color = "#00E100"}, new { category = "Not Achieved", amount = notAchieved, color = "#FF0000"} });
         }
 
+        [HttpGet("~/dashboard/patients/{id}/[controller]")]
         [HttpGet("~/dashboard/patients/{id}/[controller]/current")]
         public ActionResult Read(int id)
         {
-            AchievedGoals ag = GetAchievedGoals(id);
-            List<object> js = new List<object>();
-
-            for(int i=0; i<7; i++)
-            {//TODO Fare una get settimanale, ora solo per prova
-                js.Add(new { steps = ag.ActivitySummary.Steps + 1000*i, day = ag.ActivitySummary.Date.AddDays(i).ToShortDateString() });
+            switch (Request.Query.Count)
+            {
+                case 0:
+                    AchievedGoals ag = GetAchievedGoals(id, DateTime.Now.ToShortDateString().Replace('/', '-'));
+                    return Json(new { steps = ag.ActivitySummary.Steps, day = ag.ActivitySummary.Date.ToShortDateString() });
+                case 1:
+                    AchievedGoals agDate = GetAchievedGoals(id, Request.Query["beginDate"]);
+                    return Json(new { steps = agDate.ActivitySummary.Steps, day = agDate.ActivitySummary.Date.ToShortDateString() });
+                case 2:
+                    List<AchievedGoals> list = GetAchievedGoalsList(id, Request.Query["beginDate"], Request.Query["period"]);
+                    List<object> listJson = new List<object>();
+                    foreach (AchievedGoals agEl in list)
+                    {
+                        listJson.Add(new { steps = agEl.ActivitySummary.Steps, day = agEl.ActivitySummary.Date.ToShortDateString() });
+                    }
+                    return Json(listJson);
+                default: return null;
             }
-
-            return Json(js);
         }
+        
 
-        private AchievedGoals GetAchievedGoals(int id)
+        private AchievedGoals GetAchievedGoals(int id, string date)
         {
             var dateTimeConverter = new IsoDateTimeConverter { DateTimeFormat = "dd-MM-yyyy" };
 
-            string jsonResultGoals = Utils.Get(config.Value.GetTotalUrl() + "users/" + id + "/goals-steps-daily/current");
-            string jsonResultActivity = Utils.Get(config.Value.GetTotalUrl() + "users/" + id + "/activity-summary?date=16-07-2018");
+            string jsonResultGoals = Utils.Get(config.Value.GetTotalUrl() + "users/" + id + "/goals-steps-daily?date=" + date);
+            string jsonResultActivity = Utils.Get(config.Value.GetTotalUrl() + "users/" + id + "/activity-summary?date=16-07-2018"); //todo + date.ToShortDateString().Replace('/', '-'));
 
             JObject json = JObject.Parse(jsonResultGoals);
             JObject jsonObject = (JObject)json.GetValue("goals-steps-daily");
@@ -84,6 +94,31 @@ namespace LaywApplication.Controllers
             ag.Name = DashboardController.doctor.Patients.FirstOrDefault(x => x.Id == id).Name;
 
             return ag;
+        }
+
+        private List<AchievedGoals> GetAchievedGoalsList(int id, string beginDate, string period)
+        {
+            List<AchievedGoals> agList = new List<AchievedGoals>();
+            var dateTimeConverter = new IsoDateTimeConverter { DateTimeFormat = "dd-MM-yyyy" };
+            
+            string jsonResultActivity = Utils.Get(config.Value.GetTotalUrl() + "users/" + id + "/activity-summary?date=23-07-2018&period=" + period);//todo beginDate
+            
+            JObject jsonAct = JObject.Parse(jsonResultActivity);
+            JArray jsonArrayAct = (JArray)jsonAct.GetValue("activity-summaries");
+            
+            foreach(JObject jObj in jsonArrayAct)
+            {
+                AchievedGoals ag = new AchievedGoals();
+                ag.ActivitySummary = JsonConvert.DeserializeObject<ActivitySummary>(jObj.ToString(), dateTimeConverter);
+                
+                string jsonResultGoals = Utils.Get(config.Value.GetTotalUrl() + "users/" + id + "/goals-steps-daily?date=" + ag.ActivitySummary.Date.ToShortDateString().Replace('/','-'));
+                JObject json = (JObject)(JObject.Parse(jsonResultGoals)).GetValue("goals-steps-daily");
+                ag.GoalsStepsDaily = JsonConvert.DeserializeObject<GoalsStepsDaily>(json.ToString(), dateTimeConverter);
+
+                agList.Add(ag);
+            }
+            
+            return agList;
         }
     }
     
