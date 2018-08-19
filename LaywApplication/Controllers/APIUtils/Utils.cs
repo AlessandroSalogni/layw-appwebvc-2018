a1c87353527d1f98bbf945c8202ed7cd48cdcd8e
AlessandroSalogni <20013907@studenti.uniupo.it>
2018-08-19 15:12:03 +0200

    Aggiunta post per API async e cambiato tipo di ritorno dei metodi in JObject

--- LaywApplication/Controllers/APIUtils/Utils.cs
+++ LaywApplication/Controllers/APIUtils/Utils.cs
@@ -10,65 +10,36 @@ namespace LaywApplication.Controllers.APIUtils
 {
     public static class Utils   
     {
-        private const string dateFormat = "dd-MM-yyyy";
-
-        public static List<T> Get<T>(string baseUrl, string url, string root) where T : class, new()
-        {
-            var client = new RestClient(baseUrl);
-            var rq = new RestRequest(url, Method.GET);
-            var rs = client.Execute(rq);
-
-            JObject json = JObject.Parse(rs.Content);
-            JArray jsonArray = (JArray)json.GetValue(root);
-
-            List<T> list = new List<T>();
-            foreach (JObject obj in jsonArray)
-                list.Add(JsonConvert.DeserializeObject<T>(obj.ToString()));
-            
-            return list;
-        }
-
-        /*
-        public static string Post(string baseUrl, string url, string json)
-        {
-            var client = new RestClient(baseUrl);
-            var request = new RestRequest(url, Method.POST)
-            {
-                RequestFormat = DataFormat.Json
-            };
-            request.AddBody(json);
-
-            var response = client.Execute(request);
-            return response.Content;
-        }
-        */
-        
         private static readonly HttpClient client = new HttpClient();
 
-        public static string Get(string uri)
+        public static JObject Get(string uri)
         {
             using (var client = new WebClient())
-                return client.DownloadString(uri);//TODO gestire codice di errore
+                return JObject.Parse(client.DownloadString(uri));//TODO gestire codice di errore
         }
 
-        public async static Task<string> GetAsync(string uri)
+        public async static Task<JObject> GetAsync(string uri)
         {
             using (var client = new WebClient())
-                return await client.DownloadStringTaskAsync(uri);
+                return JObject.Parse(await client.DownloadStringTaskAsync(uri));
         }
 
-        public static string Post(string uri, string body)
+        public static JObject Post(string uri, string body)
         {
-            string result;
             using (var client = new WebClient())
             {
                 client.Headers[HttpRequestHeader.ContentType] = "application/json";
-                result = client.UploadString(uri, "POST", body);
+                return JObject.Parse(client.UploadString(uri, "POST", body));
             }
-            return result;
         }
-        
 
-
+        public async static Task<JObject> PostAsync(string uri, string body)
+        {
+            using (var client = new WebClient())
+            {
+                client.Headers[HttpRequestHeader.ContentType] = "application/json";
+                return JObject.Parse(await client.UploadStringTaskAsync(uri, "POST", body));
+            }
+        }
     }
 }
