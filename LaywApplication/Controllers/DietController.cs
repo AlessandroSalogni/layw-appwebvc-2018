a11db15003ddb28ddb61e3e8ce4950e827281953
SimoneCoppeta <20016455@studenti.uniupo.it>
2018-08-22 11:53:02 +0200

    Creato controller e modello per gestire dieta e modificato layout paziente

---
+++ LaywApplication/Controllers/DietController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LaywApplication.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LaywApplication.Controllers
+{
+    public class DietController : Controller
+    {
+        static readonly object Empty = new { };
+
+        [HttpGet ("~/dashboard/diet")]
+        public IEnumerable<Diet> Read()
+        {
+            List<Diet> dietList = new List<Diet>();
+
+            dietList.Add(new Diet { Day = "Lunedì"});
+            dietList.Add(new Diet { Day = "Martedì" });
+            dietList.Add(new Diet { Day = "Mercoledì" });
+            dietList.Add(new Diet { Day = "Giovedì" });
+            dietList.Add(new Diet { Day = "Venerdì" });
+            dietList.Add(new Diet { Day = "Sabato" });
+            dietList.Add(new Diet { Day = "Domenica" });
+
+            return dietList;
+        }
+
+        [HttpPost ("~/dashboard/diet/update")]
+        public object Update([FromBody]Diet item)
+        {
+
+            return Empty;
+        }
+    }
+}
