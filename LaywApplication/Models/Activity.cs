e1e0c39f3effb192e7840cf23160f961d5ee9388
riccardoperotti <20013787@studenti.uniupo.it>
2018-08-23 15:01:07 +0200

    Aggiunta classe activity e impostata data inziale

---
+++ LaywApplication/Models/Activity.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LaywApplication.Models
+{
+    public class Activity
+    {
+        List<HeartBeat> HeartBeats { get; set; }
+        string ActivityName { get; set; }
+        DateTime Date { get; set; }
+    }
+
+    internal class HeartBeat
+    {
+        DateTime HeartRateTime { get; set; }
+        int Value { get; set; }
+    }
+}   
