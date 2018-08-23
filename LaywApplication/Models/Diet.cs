a57bfe722def04c961f3ba4baa013e7704c35c74
SimoneCoppeta <20016455@studenti.uniupo.it>
2018-08-23 10:47:50 +0200

    Eliminati campi inutili e aggiunto campo "meals"

--- LaywApplication/Models/Diet.cs
+++ LaywApplication/Models/Diet.cs
@@ -8,11 +8,6 @@ namespace LaywApplication.Models
     public class Diet
     {
         public String Day { get; set; }
-        public String Breakfast { get; set; }
-        public String MorningSnack { get; set; }
-        public String Lunch { get; set; }
-        public String AfternoonSnack { get; set; }
-        public String Dinner { get; set; }
-        public String EveningSnack { get; set; }
+        public String Meals { get; set; }
     }
 }
