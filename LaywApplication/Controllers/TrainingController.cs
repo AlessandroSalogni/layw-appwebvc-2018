8c28ef9a4a121683f1beb2ed8a768531fde63886
AlessandroSalogni <20013907@studenti.uniupo.it>
2018-08-25 16:08:46 +0200

    Creato modello per il training e fatta read

--- LaywApplication/Controllers/TrainingController.cs
+++ LaywApplication/Controllers/TrainingController.cs
@@ -2,31 +2,63 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using LaywApplication.Configuration;
+using LaywApplication.Controllers.Utils;
 using LaywApplication.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace LaywApplication.Controllers
 {
     public class TrainingController : Controller
     {
-        static readonly object Empty = new { };
+        private static readonly object Empty = new { };
+        private readonly IOptions<ServerIP> config;
+
+        public TrainingController(IOptions<ServerIP> config)
+        {
+            this.config = config;
+        }
 
         [HttpGet("~/dashboard/training")]
         public IEnumerable<Training> Read()
-        {
+        {//todo passare id in qualche modo
+            var jsonTraining = APIUtils.Get(config.Value.GetTotalUrlUser() + "1/trainings"); //todo mettere path nel config
+            JArray jsonTrainingDayArray = (JArray)jsonTraining.GetValue("training-days");
             List<Training> trainingList = new List<Training>();
 
-            trainingList.Add(new Training { Day = "Lunedì" });
-            trainingList.Add(new Training { Day = "Martedì" });
-            trainingList.Add(new Training { Day = "Mercoledì" });
-            trainingList.Add(new Training { Day = "Giovedì" });
-            trainingList.Add(new Training { Day = "Venerdì" });
-            trainingList.Add(new Training { Day = "Sabato" });
-            trainingList.Add(new Training { Day = "Domenica" });
+            foreach (JObject jsonTrainingDay in jsonTrainingDayArray)
+            {
+                Training training = new Training { DayName = (string)jsonTrainingDay.GetValue("dayName") };
+                JArray jsonTrainingExerciseArray = (JArray)jsonTrainingDay.GetValue("trainings");
+
+                if(jsonTrainingExerciseArray != null)
+                {
+                    int i = 0;
+                    foreach (JObject jsonTrainingExercise in jsonTrainingExerciseArray)
+                    {
+                        var description = (string)jsonTrainingExercise.GetValue("description");
+                        if (i == 0)
+                            training.Exercise1 = description;
+                        else if (i == 1)
+                            training.Exercise2 = description;
+                        else if (i == 2)
+                            training.Exercise3 = description;
+                        else if (i >= 3)
+                            break;
+
+                        i++;
+                    }
+                }
+
+                trainingList.Add(training);
+            }
 
             return trainingList;
         }
-
+        
         [HttpPost("~/dashboard/training/create")]
         public object Create([FromBody]Training item)
         {
