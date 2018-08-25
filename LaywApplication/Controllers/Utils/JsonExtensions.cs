98628610242765568dc55955715b2df1bdd38427
AlessandroSalogni <20013907@studenti.uniupo.it>
2018-08-25 21:12:22 +0200

    Creato extension method per creare una lista di oggetti generici a partire da un JArray

---
+++ LaywApplication/Controllers/Utils/JsonExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
+
+namespace LaywApplication.Controllers.Utils
+{
+    public static class JsonExtensions
+    {
+        public static List<T> GetList<T>(this JArray jsonArray)
+        {
+            List<T> list = new List<T>();
+            foreach (JObject obj in jsonArray)
+                list.Add(JsonConvert.DeserializeObject<T>(obj.ToString()));
+
+            return list;
+        }
+    }
+}
