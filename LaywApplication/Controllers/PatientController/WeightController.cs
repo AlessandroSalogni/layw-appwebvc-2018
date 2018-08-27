c54c82a121ae1d993202a7b1b5f66110f5ce9fc4
riccardoperotti <20013787@studenti.uniupo.it>
2018-08-27 22:21:10 +0200

    Pagina personal data quasi completa

---
+++ LaywApplication/Controllers/PatientController/WeightController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LaywApplication.Configuration;
+using LaywApplication.Controllers.Utils;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Newtonsoft.Json.Linq;
+
+namespace LaywApplication.Controllers.PatientController
+{
+    public class WeightController : BasePatientController
+    {
+        public WeightController(IOptions<ServerIP> IPconfig, IOptions<JsonStructure> parameters) : base(IPconfig, parameters) {}
+
+        [HttpGet("~/dashboard/patients/{id}/[controller]")]
+        public async Task<IActionResult> Read(int id)
+        {
+            JObject obj = await APIUtils.GetAsync(IPconfig.GetTotalUrlUser() + id + "/weights?" + ParametersConfig.Date + "=23-06-2018"); //Request.Query["date"] //period
+            JObject weights = (JObject)obj.GetValue("weights");
+            return Json(weights);
+        }
+    }
+}
---
+++ LaywApplication/Models/PatientPersonalData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LaywApplication.Models
+{
+    public class PatientPersonalData
+    {
+        public int TotalSteps { get; set; }
+        public int TotalCalories { get; set; }
+        public Weights[] LastTwoWeights { get; set; }
+        public Double AverageFloors { get; set; }
+    }
+}
