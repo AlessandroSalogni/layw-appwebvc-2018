e07893935c88914cc14c28d22380ced2ca40456b
AlessandroSalogni <20013907@studenti.uniupo.it>
2018-09-01 18:30:05 +0200

    Creato controller da ereditare con le informazioni base

---
+++ LaywApplication/Controllers/BaseController.cs
@@ -0,0 +1,15 @@
+﻿using LaywApplication.Configuration;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Newtonsoft.Json.Converters;
+
+namespace LaywApplication.Controllers
+{
+    public class BaseController : Controller
+    {
+        protected static readonly IsoDateTimeConverter italianDateConverter = new IsoDateTimeConverter { DateTimeFormat = "dd-MM-yyyy" };
+        protected readonly ServerIP IPConfig;
+
+        protected BaseController(ServerIP IPConfig) => this.IPConfig = IPConfig;
+    }
+}
