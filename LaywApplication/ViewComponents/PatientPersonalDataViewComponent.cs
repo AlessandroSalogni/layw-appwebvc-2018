bae610144a9750ffd22aef7e59b1c9c98f3a0e3b
AlessandroSalogni <20013907@studenti.uniupo.it>
2018-09-02 23:01:34 +0200

    Refactoring della ViewComponent per i PersonalData

--- LaywApplication/ViewComponents/PatientPersonalDataViewComponent.cs
+++ LaywApplication/ViewComponents/PatientPersonalDataViewComponent.cs
@@ -24,18 +24,13 @@ namespace LaywApplication.Controllers
         
         public async Task<IViewComponentResult> InvokeAsync(Patient currentPatient)
         {
+            List<ActivitySummary> summariesMonth = await GetSummaryListAsync(currentPatient.Id, "23-06-2018", "1m"); //todo settare data oggi DateTime.Now.ToShortDateString()
+            List<ActivitySummary> summariesWeek = await GetSummaryListAsync(currentPatient.Id, "23-06-2018", "1w");
+
             var data = new PatientPersonalData
             {
                 Patient = currentPatient
             };
-
-            var jsonSummaryMonth = (JsonResult)await 
-                new ActivitySummaryController(IPConfig, JsonStructureConfig).Read(currentPatient.Id, "23-06-2018", "1m"); //todo settare data oggi DateTime.Now.ToShortDateString()
-            List<ActivitySummary> summariesMonth = JArray.Parse(jsonSummaryMonth.Value.ToString()).GetList<ActivitySummary>();
-
-            var jsonSummaryWeek = (JsonResult)await
-                new ActivitySummaryController(IPConfig, JsonStructureConfig).Read(currentPatient.Id, "23-06-2018", "1w");
-            List<ActivitySummary> summariesWeek = JArray.Parse(jsonSummaryWeek.Value.ToString()).GetList<ActivitySummary>();
 
             data.TotalSteps.Month = (from s in summariesMonth select s.Steps).Sum();
             data.TotalSteps.Week = (from s in summariesWeek select s.Steps).Sum();
@@ -46,15 +41,22 @@ namespace LaywApplication.Controllers
             data.AverageFloors.Month = (from s in summariesMonth select s.Floors).Average();
             data.AverageFloors.Week = (from s in summariesWeek select s.Floors).Average();
 
-            var jsonWeightToday = (JsonResult)await
-                new WeightController(IPConfig, JsonStructureConfig).Read(currentPatient.Id, DateTime.Now.ToShortDateString());
-            data.WeightComparison.Today = JObject.Parse(jsonWeightToday.Value.ToString()).GetObject<PatientWeight>();
-
-            var jsonWeightYesterday = (JsonResult)await 
-                new WeightController(IPConfig, JsonStructureConfig).Read(currentPatient.Id, DateTime.Now.Subtract(TimeSpan.FromDays(1)).ToShortDateString());
-            data.WeightComparison.Yesterday = JObject.Parse(jsonWeightYesterday.Value.ToString()).GetObject<PatientWeight>();
+            data.WeightComparison.Today = await GetWeightAsync(currentPatient.Id, DateTime.Now.ToShortDateString());
+            data.WeightComparison.Yesterday = await GetWeightAsync(currentPatient.Id, DateTime.Now.Subtract(TimeSpan.FromDays(1)).ToShortDateString());
 
             return View(data);
+        }
+
+        private async Task<PatientWeight> GetWeightAsync(int id, string date)
+        {
+            var jsonWeight = (JsonResult)await new WeightController(IPConfig, JsonStructureConfig).Read(id, date);
+            return JObject.Parse(jsonWeight.Value.ToString()).GetObject<PatientWeight>();
+        }
+
+        private async Task<List<ActivitySummary>> GetSummaryListAsync(int id, string date, string period)
+        {
+            var jsonSummary = (JsonResult)await new ActivitySummaryController(IPConfig, JsonStructureConfig).Read(id, date, period);
+            return JArray.Parse(jsonSummary.Value.ToString()).GetList<ActivitySummary>();
         }
     }
 }
