ae6573709660b096a854428eb8f8ca5e031a87b2
AlessandroSalogni <20013907@studenti.uniupo.it>
2018-09-02 23:59:40 +0200

    Creato controller base per i json

---
+++ LaywApplication/Controllers/BaseJsonController.cs
@@ -0,0 +1,19 @@
+﻿using LaywApplication.Configuration;
+
+namespace LaywApplication.Controllers
+{
+    public abstract class BaseJsonController : BaseController
+    {
+        protected readonly QueryParams QueryParamsConfig;
+        protected readonly AdditionalPath AdditionalPathConfig;
+        protected readonly JsonData JsonDataConfig;
+
+        public BaseJsonController(ServerIP IPConfig, JsonStructure jsonStructureConfig, JsonData jsonDataConfig) 
+            : base(IPConfig)
+        {
+            QueryParamsConfig = jsonStructureConfig.QueryParams;
+            AdditionalPathConfig = jsonStructureConfig.AdditionalPath;
+            JsonDataConfig = jsonDataConfig;
+        }
+    }
+}
