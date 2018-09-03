ebf49f919de950f2884d4c05b34d5489f705f99f
AlessandroSalogni <20013907@studenti.uniupo.it>
2018-09-03 09:29:16 +0200

    Tolta direttiva using inutile

--- LaywApplication/Controllers/PatientController/WeightController.cs
+++ LaywApplication/Controllers/PatientController/WeightController.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using LaywApplication.Configuration;
 using LaywApplication.Controllers.Utils;
 using LaywApplication.Models;
