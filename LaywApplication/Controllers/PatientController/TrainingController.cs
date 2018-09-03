f3776b7d5079eb670730dff509dd3412e9ecd0e6
AlessandroSalogni <20013907@studenti.uniupo.it>
2018-09-03 14:17:18 +0200

    Eliminata direttiva using inutile

--- LaywApplication/Controllers/PatientController/TrainingController.cs
+++ LaywApplication/Controllers/PatientController/TrainingController.cs
@@ -6,7 +6,6 @@ using LaywApplication.Models;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
-using Newtonsoft.Json.Serialization;
 
 namespace LaywApplication.Controllers
 {
