a144c12102bc6e9e5d81e95354d6c9778a426251
AlessandroSalogni <20013907@studenti.uniupo.it>
2018-09-03 13:45:04 +0200

    Aggiunti componenti utili a tutte le classi che estendono le classi base

--- LaywApplication/Controllers/BaseController.cs
+++ LaywApplication/Controllers/BaseController.cs
@@ -8,6 +8,8 @@ namespace LaywApplication.Controllers
     public class BaseController : Controller
     {
         protected static readonly IsoDateTimeConverter italianDateConverter = new IsoDateTimeConverter { DateTimeFormat = "dd-MM-yyyy" };
+        protected static readonly object Empty = new { };
+
         protected readonly ServerIP IPConfig;
 
         protected BaseController(ServerIP IPConfig) => this.IPConfig = IPConfig;
--- LaywApplication/Controllers/BaseJsonController.cs
+++ LaywApplication/Controllers/BaseJsonController.cs
@@ -1,9 +1,16 @@
 ﻿using LaywApplication.Configuration;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
 
 namespace LaywApplication.Controllers
 {
     public abstract class BaseJsonController : BaseController
     {
+        protected static readonly JsonSerializerSettings serializerSettings = new JsonSerializerSettings
+        {
+            ContractResolver = new CamelCasePropertyNamesContractResolver(),
+        };
+
         protected readonly QueryParams QueryParamsConfig;
         protected readonly AdditionalPath AdditionalPathConfig;
         protected readonly JsonData JsonDataConfig;
