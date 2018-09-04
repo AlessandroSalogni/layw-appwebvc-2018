73658af0ccb47e7e00f8f4a2080f251e541f311e
AlessandroSalogni <20013907@studenti.uniupo.it>
2018-09-04 12:34:18 +0200

    Sistemato controller per il GoalStepsDaily

--- LaywApplication/Controllers/PatientController/GoalStepsDailyController.cs
+++ LaywApplication/Controllers/PatientController/GoalStepsDailyController.cs
@@ -1,47 +1,36 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using LaywApplication.Configuration;
 using LaywApplication.Controllers.Utils;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Options;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
-using Newtonsoft.Json.Serialization;
 
 namespace LaywApplication.Controllers.PatientController
 {
-    public class GoalStepsDailyController : BasePatientController
+    public class GoalStepsDailyController : BaseJsonController
     {
-        private static readonly object Empty = new { };
-        public GoalStepsDailyController(IOptions<ServerIP> IPconfig, IOptions<JsonStructure> parameters) : base(IPconfig, parameters)
-        {}
+        public GoalStepsDailyController(ServerIP IPConfig, JsonStructure jsonStructureConfig) 
+            : base(IPConfig, jsonStructureConfig, jsonStructureConfig.GoalsStepsDaily) { }
 
         [HttpGet("~/dashboard/patients/{id}/[controller]")]
-        public async Task<Models.GoalsStepsDaily> Read(int id)
+        public async Task<Models.GoalsStepsDaily> Read(int id, string date)
         {
-            JObject obj = await APIUtils.GetAsync(IPconfig.GetTotalUrlUser() + id + "/goals-steps-daily/current"); //Request.Query["date"] //period
-            return ((JObject)obj.GetValue("goals-steps-daily")).GetObject<Models.GoalsStepsDaily>();
+            JObject goalStepsJson = await APIUtils.GetAsync(IPConfig.GetTotalUrlUser() + id +
+                JsonDataConfig.Url + EndUrlDate(Request, date));
+            return ((JObject)goalStepsJson[JsonDataConfig.Root]).GetObject<Models.GoalsStepsDaily>();
         }
 
         [HttpPost("~/dashboard/patients/{id}/[controller]/update")]
         public async Task<object> Update(int id, [FromBody]Models.GoalsStepsDaily item)
         {
-            var serializerSettings = new JsonSerializerSettings
+            var GoalsStepsDailyConfig = JsonDataConfig as GoalsStepsDaily;
+            var goalStepsJson = new JObject
             {
-                ContractResolver = new CamelCasePropertyNamesContractResolver(),
-            };
-            
-            JObject jsonStepsObj = JObject.Parse(JsonConvert.SerializeObject(item, serializerSettings));
-
-            var jsonSteps = new JObject
-            {
-                { "goals-steps", jsonStepsObj }
+                { GoalsStepsDailyConfig.RootUpdate, JObject.Parse(JsonConvert.SerializeObject(item, serializerSettings)) }
             };
 
-            await APIUtils.PostAsync(IPconfig.GetTotalUrlUser() + id + "/goals-steps?period=daily", jsonSteps.ToString());
-
+            await APIUtils.PostAsync(IPConfig.GetTotalUrlUser() + id + GoalsStepsDailyConfig.UrlUpdate + "?"
+                + QueryParamsConfig.Period + "=" + GoalsStepsDailyConfig.Period, goalStepsJson.ToString());
             return Empty;
         }
     }
