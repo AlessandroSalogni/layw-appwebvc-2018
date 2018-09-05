255f55e63fa84017083c9a41b37a83e538aca4a9
AlessandroSalogni <20013907@studenti.uniupo.it>
2018-09-05 15:55:34 +0200

    Sistemata classe per gestire confronto tra goal e real data

---
+++ LaywApplication/Controllers/PatientController/Abstract/GoalStatisticsAbstractContoller.cs
@@ -0,0 +1,55 @@
+﻿using LaywApplication.Configuration;
+using LaywApplication.Models;
+using LaywApplication.Models.Abstract;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace LaywApplication.Controllers.PatientController.Abstract
+{
+    public abstract class GoalStatisticsController<TType, TModelGoal, TModelRealData> : BaseJsonController
+      where TType : IComparable<TType>
+      where TModelGoal : GoalAbstract<TType>
+      where TModelRealData : RealDataAbstract<TType>
+    {
+        protected readonly BaseJsonReadController<TModelGoal> GoalController;
+        protected readonly BaseJsonReadController<TModelRealData> RealDataController;
+
+        public GoalStatisticsController(ServerIP IPConfig, JsonStructure jsonStructureConfig,
+            BaseJsonReadController<TModelGoal> goalController,
+            BaseJsonReadController<TModelRealData> realDataController)
+            : base(IPConfig, jsonStructureConfig)
+        {
+            GoalController = goalController;
+            RealDataController = realDataController;
+        }
+
+        [HttpGet]
+        public async Task<List<GoalRealDataCompare<TType>>> Read(int id, string beginDate, string period)
+        {
+            var goalReadDataCompareList = new List<GoalRealDataCompare<TType>>();
+            List<TModelRealData> realDataList = await RealDataController.Read(id, beginDate, period);
+
+            TModelGoal goal = null;
+            TModelRealData realData = null;
+
+            for (int i = realDataList.Count - 1; i > -1; i--)
+            {
+                realData = realDataList[i];
+                if (goal == null || DateTime.Compare(goal.Date, realData.Date) > 0)
+                    goal = await GoalController.Read(id, realData.Date.ToString(italianDateFormat));
+
+                goalReadDataCompareList.Insert(0, new GoalRealDataCompare<TType>
+                {
+                    Goal = goal.Goal,
+                    RealData = realData.RealData,
+                    Date = realData.Date.ToString(italianDateFormat)
+                });
+            }
+
+            return goalReadDataCompareList;
+        }
+    }
+
+}
---
+++ LaywApplication/Controllers/PatientController/GoalStatisticsController.cs
@@ -0,0 +1,38 @@
+﻿using LaywApplication.Configuration;
+using LaywApplication.Controllers.PatientController;
+using LaywApplication.Controllers.PatientController.Abstract;
+using LaywApplication.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LaywApplication.Controllers
+{
+  
+    [Route("~/dashboard/patients/{id}/[controller]")]
+    public class GoalStepsDailyStatisticsController 
+        : GoalStatisticsController<int, GoalStepsDaily, ActivitySummarySteps>
+    {
+        public GoalStepsDailyStatisticsController(ServerIP IPConfig, JsonStructure jsonStructureConfig) 
+            : base(IPConfig, jsonStructureConfig, 
+                  new GoalStepsDailyController(IPConfig, jsonStructureConfig),
+                  new ActivitySummaryStepsController(IPConfig, jsonStructureConfig)) { }
+    }
+
+    [Route("~/dashboard/patients/{id}/[controller]")]
+    public class GoalWeightStatisticsController : GoalStatisticsController<double, GoalWeight, PatientWeight>
+    {
+        public GoalWeightStatisticsController(ServerIP IPConfig, JsonStructure jsonStructureConfig)
+            : base(IPConfig, jsonStructureConfig,
+                  new GoalWeightController(IPConfig, jsonStructureConfig),
+                  new WeightController(IPConfig, jsonStructureConfig)) { }
+    }
+
+    [Route("~/dashboard/patients/{id}/[controller]")]
+    public class GoalCaloriesOutStatisticsController 
+        : GoalStatisticsController<int, Models.GoalCaloriesOut, ActivitySummaryCalories>
+    {
+        public GoalCaloriesOutStatisticsController(ServerIP IPConfig, JsonStructure jsonStructureConfig)
+            : base(IPConfig, jsonStructureConfig,
+                  new GoalCaloriesOutController(IPConfig, jsonStructureConfig),
+                  new ActivitySummaryCaloriesController(IPConfig, jsonStructureConfig)) { }
+    }
+}
