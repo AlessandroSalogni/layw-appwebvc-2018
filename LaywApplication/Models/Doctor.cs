ee71c53cb8e33ed7bc4b0ba80ac628e6c8237229
AlessandroSalogni <20013907@studenti.uniupo.it>
2018-09-05 22:54:22 +0200

    Sistemata modello per il doctor

--- LaywApplication/Models/Doctor.cs
+++ LaywApplication/Models/Doctor.cs
@@ -1,26 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 
 namespace LaywApplication.Models
 {
     public class Doctor
     {
-        public Doctor(string EMail, string Name, Uri Image)
-        {
-            this.EMail = EMail;
-            this.Name = Name;
-            this.Image = Image;
-            Patients = new List<Patient>();
-        }
-        public string EMail { get; }
-        public string Name { get; }
-        public Uri Image { get; }
-        public List<Patient> Patients { get; set; } //todo creo un'altra lista nel controller!!!!
-
-        //public Patient GetPatientWithId(int id)
-        //{
-        //    return Patients.FirstOrDefault(x => x.Id == id);
-        //}
+        public string EMail { get; set; }
+        public string Name { get; set; }
+        public Uri Image { get; set; }
+        public List<Patient> Patients { get; set; }
     }
 }
