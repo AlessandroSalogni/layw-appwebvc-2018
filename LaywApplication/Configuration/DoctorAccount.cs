1ea327fc3a4ca778ff6823ce4e34b7f93e6111e3
AlessandroSalogni <20013907@studenti.uniupo.it>
2018-09-05 22:43:29 +0200

    Cambiata nome property

--- LaywApplication/Configuration/DoctorAccount.cs
+++ LaywApplication/Configuration/DoctorAccount.cs
@@ -4,7 +4,7 @@
     {
         public string Email { get; set; }
         public string Name { get; set; }
-        public string Image { get; set; }
+        public string ImageUri { get; set; }
 
     }
 }
