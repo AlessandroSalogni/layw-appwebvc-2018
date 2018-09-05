0e5dabd7556366e3d95bd1f056ed23037db62d9f
AlessandroSalogni <20013907@studenti.uniupo.it>
2018-09-05 22:38:52 +0200

    Aggiunta configurazione per url dell'account del dottore

---
+++ LaywApplication/Configuration/DoctorAccount.cs
@@ -0,0 +1,10 @@
+﻿namespace LaywApplication.Configuration
+{
+    public class DoctorAccount
+    {
+        public string Email { get; set; }
+        public string Name { get; set; }
+        public string Image { get; set; }
+
+    }
+}
