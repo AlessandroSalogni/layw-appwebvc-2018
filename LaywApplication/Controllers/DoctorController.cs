92e43f5ad6bda166f380106c48f2dc02f7fd977c
AlessandroSalogni <20013907@studenti.uniupo.it>
2018-09-06 00:24:59 +0200

    Creato controller per il dottore

---
+++ LaywApplication/Controllers/DoctorController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using LaywApplication.Configuration;
+using LaywApplication.Controllers.Utils;
+using LaywApplication.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace LaywApplication.Controllers
+{
+    [Route("~/dashboard/doctors")]
+    public class DoctorController : BaseJsonController
+    {
+        public DoctorController(ServerIP IPConfig, JsonStructure jsonStructure) 
+            : base(IPConfig, jsonStructure, jsonStructure.Doctor) { }
+
+        [HttpGet]
+        public List<Models.Doctor> Read()
+        {
+            return null;
+        }
+
+        [HttpGet("{email}")]
+        public async Task<List<Patient>> Read(string email)
+        {
+            var doctorConfig = JsonDataConfig as Configuration.Doctor;
+
+            JObject patientsJson = await APIUtils.GetAsync(IPConfig.GetTotalUrl() + doctorConfig.UrlPatients + 
+                "?" + QueryParamsConfig.DoctorId + "=" + email);
+            return (patientsJson == null) ? new List<Patient>() :
+                ((JArray)patientsJson[doctorConfig.RootPatients]).GetList<Patient>();
+        }
+
+        [HttpPut("{email}/update")]
+        public object Update(string email, [FromBody]List<Patient> item)
+        {
+            return Empty;
+        }
+
+        [HttpPost("create")]
+        public async Task<object> Create([FromBody]Models.Doctor item)
+        {
+            var doctorJson = new JObject
+            {
+                { JsonDataConfig.Root , JObject.Parse(JsonConvert.SerializeObject(item, serializerSettings)) }
+            };
+
+            await APIUtils.PostAsync(IPConfig.GetTotalUrl() + JsonDataConfig.Url, doctorJson.ToString());
+            return Empty;
+        }
+    }
+}
