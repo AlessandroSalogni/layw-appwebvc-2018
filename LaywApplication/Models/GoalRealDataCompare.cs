5c33475c39f6209888cab4d9fb731230c913659b
AlessandroSalogni <20013907@studenti.uniupo.it>
2018-09-05 12:47:27 +0200

    Creata classe generica per fare confronti tra goal e real data

---
+++ LaywApplication/Models/GoalRealDataCompare.cs
@@ -0,0 +1,9 @@
+﻿namespace LaywApplication.Models
+{
+    public class GoalRealDataCompare<TType>
+    {
+        public TType RealData { get; set; }
+        public TType Goal { get; set; }
+        public string Date { get; set; }
+    }
+}
