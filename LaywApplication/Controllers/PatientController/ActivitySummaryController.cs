f5ee7d52ff3bce7e639f814245cbb8a3aa3085cb
AlessandroSalogni <20013907@studenti.uniupo.it>
2018-09-05 12:41:10 +0200

    Aggiunt controller per steps e caloris in ActivitySummary

--- LaywApplication/Controllers/PatientController/ActivitySummaryController.cs
+++ LaywApplication/Controllers/PatientController/ActivitySummaryController.cs
@@ -9,4 +9,18 @@ namespace LaywApplication.Controllers.PatientController
         public ActivitySummaryController(ServerIP IPConfig, JsonStructure jsonStructureConfig) 
             : base(IPConfig, jsonStructureConfig, jsonStructureConfig.ActivitySummary) { }
     }
+
+    [Route("~/dashboard/patients/{id}/[controller]")]
+    public class ActivitySummaryStepsController : BaseJsonReadController<Models.ActivitySummarySteps>
+    {
+        public ActivitySummaryStepsController(ServerIP IPConfig, JsonStructure jsonStructureConfig)
+            : base(IPConfig, jsonStructureConfig, jsonStructureConfig.ActivitySummary) { }
+    }
+
+    [Route("~/dashboard/patients/{id}/[controller]")]
+    public class ActivitySummaryCaloriesController : BaseJsonReadController<Models.ActivitySummaryCalories>
+    {
+        public ActivitySummaryCaloriesController(ServerIP IPConfig, JsonStructure jsonStructureConfig)
+            : base(IPConfig, jsonStructureConfig, jsonStructureConfig.ActivitySummary) { }
+    }
 }
