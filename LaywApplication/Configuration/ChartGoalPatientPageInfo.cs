9b797e40db3029f5d75b46bd657d17c3704948e7
AlessandroSalogni <20013907@studenti.uniupo.it>
2018-09-06 15:44:31 +0200

    Modificate configurazioni per i grafici dei goal dei pazienti

--- LaywApplication/Configuration/ChartGoalPatientPageInfo.cs
+++ LaywApplication/Configuration/ChartGoalPatientPageInfo.cs
@@ -2,21 +2,19 @@
 {
     public class ChartGoalPatientPageInfo
     {
-        public WeightChartInfo WeightChartInfo { get; set; }
-        public StepsDailyChartInfo StepsDailyChartInfo { get; set; }
-        public CaloriesOutChartInfo CaloriesOutChartInfo { get; set; }
+        public WeightPatientPageChartInfo WeightChartInfo { get; set; }
+        public StepsDailyPatientPageChartInfo StepsDailyChartInfo { get; set; }
+        public CaloriesOutPatientPageChartInfo CaloriesOutChartInfo { get; set; }
     }
 
-    public class ChartInfo
+    public class ChartPatientPageInfo : ChartInfo
     {
-        public string Title { get; set; }
-        public string Controller { get; set; }
         public string ControllerGoal { get; set; }
         public string[] LegendTitle { get; set; }
         public int AxisXInterval { get; set; }
     }
 
-    public class WeightChartInfo : ChartInfo { }
-    public class StepsDailyChartInfo : ChartInfo { }
-    public class CaloriesOutChartInfo : ChartInfo { }
+    public class WeightPatientPageChartInfo : ChartPatientPageInfo { }
+    public class StepsDailyPatientPageChartInfo : ChartPatientPageInfo { }
+    public class CaloriesOutPatientPageChartInfo : ChartPatientPageInfo { }
 }
