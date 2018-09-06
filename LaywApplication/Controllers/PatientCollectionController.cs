0d31620092fafe202eca5642e1e3a568ef5bc2b1
riccardoperotti <20013787@studenti.uniupo.it>
2018-09-06 15:08:57 +0200

    Creato controller per il paziente

---
+++ LaywApplication/Controllers/PatientCollectionController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LaywApplication.Configuration;
+using LaywApplication.Controllers.PatientController;
+using LaywApplication.Controllers.Utils;
+using LaywApplication.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+
+namespace LaywApplication.Controllers
+{
+    [Route("~/dashboard/patients")]
+    public class PatientCollectionController : BaseJsonController
+    {
+        public PatientCollectionController(ServerIP IPConfig, JsonStructure jsonStructureConfig) : base(IPConfig, jsonStructureConfig, jsonStructureConfig.Patient) {}
+
+        [HttpGet]
+        public async Task<List<Models.Patient>> Read()
+        {
+            var patientConfig = JsonDataConfig as Configuration.Patient;
+
+            JObject jsonPatients = await APIUtils.GetAsync(IPConfig.GetTotalUrl() + patientConfig.Url);
+            List<Models.Patient> patients = ((JArray)jsonPatients[patientConfig.RootPatients]).GetList<Models.Patient>();
+            
+            return patients;
+        }
+    }
+}
