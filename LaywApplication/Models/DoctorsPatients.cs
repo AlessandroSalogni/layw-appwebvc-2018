57982eb3ac7e9aa058c63bcac2666689014ec79d
riccardoperotti <20013787@studenti.uniupo.it>
2018-09-07 14:37:15 +0200

    Modello che raccoglie tutti i dottori e tutti i pazienti del sistema

---
+++ LaywApplication/Models/DoctorsPatients.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LaywApplication.Models
+{
+    public class DoctorsPatients
+    {
+        public List<Doctor> Doctors { get; set; }
+        public List<Patient> Patients { get; set; }
+    }
+}
