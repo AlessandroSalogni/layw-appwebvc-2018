f913815a1e09d0decc201210d713ccbee35c5d3c
AlessandroSalogni <20013907@studenti.uniupo.it>
2018-09-09 21:05:33 +0200

    Aggiunta api per il singolo paziente

--- LaywApplication/Controllers/Services/PatientController.cs
+++ LaywApplication/Controllers/Services/PatientController.cs
@@ -11,16 +11,25 @@ namespace LaywApplication.Controllers.Services
     [Route("~/dashboard/patients")]
     public class PatientController : BaseJsonController
     {
-        public PatientController(ServerIP IPConfig, JsonStructure jsonStructureConfig) : base(IPConfig, jsonStructureConfig, jsonStructureConfig.Patient) {}
+        public PatientController(ServerIP IPConfig, JsonStructure jsonStructureConfig) 
+            : base(IPConfig, jsonStructureConfig, jsonStructureConfig.Patient) { }
 
         [HttpGet]
         public async Task<List<Models.Patient>> Read()
         {
-            var patientConfig = JsonDataConfig as Configuration.Patient;
+            var patientConfig = JsonDataConfig as Patient;
 
-            JObject patientsJson = await APIUtils.GetAsync(IPConfig.GetTotalUrl() + patientConfig.Url);            
+            JObject patientsJson = await APIUtils.GetAsync(IPConfig.GetTotalUrl() + patientConfig.Url);
             return (patientsJson == null) ? new List<Models.Patient>() :
                 ((JArray)patientsJson[patientConfig.RootPatients]).GetList<Models.Patient>();
+        }
+
+        [HttpGet("{id}/data")]
+        public async Task<Models.Patient> Read(int id)
+        {
+            JObject patientsJson = await APIUtils.GetAsync(IPConfig.GetTotalUrl() + JsonDataConfig.Url + "/" + id);
+            return (patientsJson == null) ? null :
+                ((JObject)patientsJson[JsonDataConfig.Root]).GetObject<Models.Patient>();
         }
     }
 }
