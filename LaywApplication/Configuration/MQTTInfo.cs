92cbe01aae8fe767d7c4242dd05c4aa27bfb0bb9
AlessandroSalogni <20013907@studenti.uniupo.it>
2018-09-09 21:03:56 +0200

    Aggiunta configuarzione per MQTT

---
+++ LaywApplication/Configuration/MQTTInfo.cs
@@ -0,0 +1,10 @@
+﻿namespace LaywApplication.Configuration
+{
+    public class MQTTInfo
+    {
+        public string BrokerHostName { get; set; }
+        public int BrokerPort { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
